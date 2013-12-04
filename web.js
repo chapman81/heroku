119e91057742aaee16fbc4762bb7deba032ac2ca
OpenShift guest <chapman@colabeo.com>
2013-12-04 18:35:11 -0500

    test

--- web.js
+++ web.js
@@ -4,14 +4,14 @@
 
 /**
  * new world
- * /
+ */
 
 var express = require("express");
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello World! nodeJS');
 });
 
 var port = process.env.PORT || 5000;
