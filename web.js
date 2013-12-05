f9cf12f3fa458d5848f30f7bc128220f7ec192a2
OpenShift guest <chapman@colabeo.com>
2013-12-04 19:26:55 -0500

    test

--- web.js
+++ web.js
@@ -11,10 +11,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! nodeJS');
+  response.send('Hello World! nodeJS via heroku');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
